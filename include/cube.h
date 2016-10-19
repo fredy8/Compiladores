6edcac323d6a21f1408204615c5721227518d08e
Alberto Astiazaran <pmdmaster@hotmail.com>
2016-10-18 20:20:54 -0500

    Move cube to header file.

---
+++ include/cube.h
@@ -0,0 +1,47 @@
+class Cube {
+public:
+  static std::string getOperationType(std::string oper, std::string type1, std::string type2) {
+    int operType = operatorType(type);
+    switch (operType) {
+      case 0:
+        if (type1 == "int" && type2 == "int") 
+          return "int";
+        if (type1 == "int" || type1 == "float") && (type2 == "float" || type2 == "int")
+          return "float";
+        return "";
+      case 1:
+        if (type1 == "int" && type2 == "int")
+          return "int";
+        return "";
+      case 2:
+        if (type1 == "int" || type1 == "float") && (type2 == "float" || type2 == "int")
+          return "bool";
+        return "";
+      case 3:
+        if (type1 == "bool" && type2 == "bool") 
+          return "bool";
+        return "";
+      case 4:
+        if (type1 == "bool")
+          return "bool";
+        return "";
+      default:
+        return "";
+    };
+  }
+
+private:
+  int operatorType(std::string oper) {
+    if (oper == '+' || oper == '-' || oper == '*' || oper == '/')
+      return 0;
+    if (oper == '%')
+      return 1;
+    if (oper == '>' || oper == '<' || oper == '<=' || oper == '>=' || oper == '==')
+      return 2;
+    if (oper == '&&' || oper == '||')
+      return 3;
+    if (oper == '!')
+      return 4;
+    return -1;
+  }
+};
--- src/cube.h
+++
@@ -1,4 +0,0 @@
-class Cube {
-public:
-  static std::string getOperationType(std::string oper, std::string type1, std::string type2);
-};
